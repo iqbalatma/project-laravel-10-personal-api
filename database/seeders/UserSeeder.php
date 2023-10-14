f1181c133801232d20095ba1c6272565ee8ff8f8
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2023-10-14 22:32:09 +0700

    user and profile seeder done

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -43,5 +42,8 @@ class UserSeeder extends Seeder
                 $user->profile()->create($profile);
             }
         }
+
+
+        User::factory()->count(100)->create();
     }
 }
